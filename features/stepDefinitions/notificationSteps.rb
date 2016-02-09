2100d3c53f3a0a2bf51821f12903e465a4d2710e
doubletgeekedup <tat081000@gmail.com>
2016-02-09 17:45:17 -0600

    Update notificationSteps.rb

--- features/stepDefinitions/notificationSteps.rb
+++ features/stepDefinitions/notificationSteps.rb
@@ -1,18 +1,16 @@
 
-When(/^I ask to be notified when size (.*) width (.*) is in stock$/) do |size, width|
+When(/^I ask to be notified when size (.*) is in stock$/) do |size,|
   within_window(windows.last) do
     fill_in("email", with: "shopper@shoes.com")
     select '12'
-    select 'EE - Wide'
     click_button_with_text("NOTIFY ME!")
   end
 end
 
-Then(/^I should see a notification message for size (.*) width (.*)$/) do |size, width|
+Then(/^I should see a notification message for size (.*) $/) do |size|
   within_window(windows.last) do
     expect(page).to have_content("THANK YOU!")
     expect(page).to have_content("We'll send you an email at shopper@shoes.com if the following combination becomes available:")
     expect(page).to have_content("SIZE:  #{size}")
-    expect(page).to have_content("WIDTH:  #{width}")
   end
 end
