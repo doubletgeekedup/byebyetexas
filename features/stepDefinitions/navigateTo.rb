118f8f868822924f60f36f3cef29c6292a5015db
doubletgeekedup <tat081000@gmail.com>
2016-02-09 17:50:10 -0600

    Update navigateTo.rb

--- features/stepDefinitions/navigateTo.rb
+++ features/stepDefinitions/navigateTo.rb
@@ -1,3 +1,16 @@
 Given(/^I am at Zappos\.com$/) do
   visit "http://www.zappos.com" 
 end
+
+When(/^I am on the (.*) page$/) do |path|
+  case path
+  when "Men's Shoes"
+    visit "/mens-shoes"
+  when "Athletic Men's Shoes"
+    visit "/men-shoes~1y"
+  when "Inov-8 Men's Shoes"
+    visit "/men-shoes~1y#!/inov-8-men-shoes/CK_XAToC6xJaArsMwAEC4gIEGAcBCw.zso?s=goliveRecentSalesStyle/desc/"
+  else
+    visit "/"
+  end
+end
